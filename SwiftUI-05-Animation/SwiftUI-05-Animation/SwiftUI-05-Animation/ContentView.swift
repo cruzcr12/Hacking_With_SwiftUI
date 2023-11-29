eb1e493bc43978983effd74f42a973cdfc9b8db6
Esteban Cruz Hidalgo <cruzcr6@gmail.com>
2023-11-29 07:43:49 -0600

    Adding project 05 for Animations

---
+++ SwiftUI-05-Animation/SwiftUI-05-Animation/SwiftUI-05-Animation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-05-Animation
+//
+//  Created by Esteban Alonso Cruz Hidalgo on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-05-Animation/SwiftUI-05-Animation/SwiftUI-05-Animation/SwiftUI_05_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_05_AnimationApp.swift
+//  SwiftUI-05-Animation
+//
+//  Created by Esteban Alonso Cruz Hidalgo on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_05_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
